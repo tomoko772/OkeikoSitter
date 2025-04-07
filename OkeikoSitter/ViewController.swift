d82682ff58dec41ec811dec25de8d1680b1be4d3
Tomoko Tajima <karintococonut777@gmail.com>
2025-04-07 22:56:39 +0900

    Initial Commit

---
+++ OkeikoSitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OkeikoSitter
+//
+//  Created by Tomoko T. Nakao on 2025/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OkeikoSitterTests/OkeikoSitterTests.swift
@@ -0,0 +1,17 @@
+//
+//  OkeikoSitterTests.swift
+//  OkeikoSitterTests
+//
+//  Created by Tomoko T. Nakao on 2025/04/07.
+//
+
+import Testing
+@testable import OkeikoSitter
+
+struct OkeikoSitterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
