10c5dc8657587fe2ccd1ce6aeac8440f0c783680
Tomoko Tajima <karintococonut777@gmail.com>
2025-04-11 22:22:52 +0900

    設定ボタン用アイコンをナビゲーションバーに追加

--- OkeikoSitter/MainViewController.swift
+++ OkeikoSitter/MainViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
+/// Home画面
 class MainViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    // MARK: - IBOutlets
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureNavigationBar()
     }
-
-
+    
+    // MARK: - IBActions
+    
+    // MARK: - Other Methods
+    
+    private func configureNavigationBar() {
+        
+        // 画像ビューを作成
+        let imageView = UIImageView(image: UIImage(named: "ic_setting"))
+        imageView.contentMode = .scaleAspectFit
+        
+        // 画像のサイズを適切に設定
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: 38, height: 38))
+        imageView.frame = titleView.bounds
+        titleView.addSubview(imageView)
+        
+        // ナビゲーションアイテムのタイトルビューに設定
+        self.navigationItem.titleView = titleView
+    }
 }
 
