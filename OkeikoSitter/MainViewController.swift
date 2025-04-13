e0ed2f4f12fd3d23298f127282f4400649741dc2
Tomoko Tajima <karintococonut777@gmail.com>
2025-04-13 11:32:00 +0900

    MainViewContorollerへの変更をリセット

--- OkeikoSitter/MainViewController.swift
+++ OkeikoSitter/MainViewController.swift
@@ -7,38 +7,11 @@
 
 import UIKit
 
-/// Home画面
 class MainViewController: UIViewController {
-    
-    // MARK: - Properties
-    
-    // MARK: - IBOutlets
-    
-    // MARK: - View Life-Cycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureNavigationBar()
-    }
-    
-    // MARK: - IBActions
-    
-    // MARK: - Other Methods
-    
-    private func configureNavigationBar() {
-        
-        // 画像ビューを作成
-        let imageView = UIImageView(image: UIImage(named: "ic_setting"))
-        imageView.contentMode = .scaleAspectFit
-        
-        // 画像のサイズを適切に設定
-        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: 38, height: 38))
-        imageView.frame = CGRect(x: 100, y: 0, width: 38, height: 38) // x座標を調整して右に寄せる
-        //imageView.frame = titleView.bounds
-        titleView.addSubview(imageView)
-        // ナビゲーションアイテムのタイトルビューに設定
-        self.navigationItem.titleView = titleView
     }
 }
 
