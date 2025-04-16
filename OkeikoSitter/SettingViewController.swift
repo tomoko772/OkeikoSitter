e0f118c38d9e13ad4f16d3b3c4c4079bdcf0d23f
Tomoko Tajima <karintococonut777@gmail.com>
2025-04-17 06:32:10 +0900

    SettingViewController.swiftとUsersViewCOntroller.swiftを削除

--- OkeikoSitter/SettingViewController.swift
+++
@@ -1,7 +0,0 @@
-//
-//  SettingViewController.swift
-//  OkeikoSitter
-//
-//  Created by Tomoko T. Nakao on 2025/04/15.
-//
-
--- OkeikoSitter/UsersViewController.swift
+++
@@ -1,7 +0,0 @@
-//
-//  UsersViewController.swift
-//  OkeikoSitter
-//
-//  Created by Tomoko T. Nakao on 2025/04/15.
-//
-
