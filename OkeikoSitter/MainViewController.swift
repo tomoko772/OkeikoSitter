d8e63c2a0cdf345f783a2546a8b439f0549549d4
Tomoko Tajima <karintococonut777@gmail.com>
2025-05-17 14:35:06 +0900

    押した際にイベントが発生する部品をIBAction接続

--- OkeikoSitter/MainViewController.swift
+++ OkeikoSitter/MainViewController.swift
@@ -12,28 +12,51 @@ final class MainViewController: UIViewController {
     
     // MARK: - IBOutlets
     
+    /// ユーザー画像
     @IBOutlet private weak var userImage: UIImageView!
     
+    /// ユーザーネーム
     @IBOutlet private weak var userName: UIView!
     
-    @IBOutlet private weak var goal: UIView!
+    /// 目標（課題）の内容
+    @IBOutlet weak var task: UILabel!
     
+    /// 目標（課題）達成時にもらえるポイント数
     @IBOutlet private weak var dailyPoint: UILabel!
     
+    /// ボーナスポイント数
     @IBOutlet private weak var bonusPoint: UIButton!
     
+    /// 現在のポイント数の表示
     @IBOutlet private weak var currentPoint: UILabel!
     
+    /// 目標ポイント数
     @IBOutlet private weak var goalPoint: UILabel!
     
-    @IBOutlet private weak var remainingDays: UIButton!
+    /// 残りの日数
+    @IBOutlet weak var remainingDays: UILabel!
     
-    @IBOutlet private weak var calendar: UIButton!
-    
+    /// GIF画像を表示するためにIBOutlet接続
     @IBOutlet private weak var gifImage: UIImageView!
     
+    /// GIF画像を表示するためにIBOutlet接
     @IBOutlet private weak var gifImage2: UIImageView!
     
+    /// 目標が表示されたボタンを押した時に呼ばれる関数
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+    
+    /// ボーナスボタンを押した時に呼ばれる関数
+    @IBAction func addBonusButtonTapped(_ sender: Any) {
+    }
+    
+    /// 残り日数が表示されたボタンを押した時に呼ばれる関数
+    @IBAction func calendarButtonTapped(_ sender: Any) {
+    }
+    
+    /// ご褒美ボタンを押した時に呼ばれる関数
+    @IBAction func presentButtonTapped(_ sender: Any) {
+    }
     
     // MARK: - View Life-Cycle Methods
     
