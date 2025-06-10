76cc58296b32d19d572d2bf1349b5a740cbf9260
Tomoko Tajima <karintococonut777@gmail.com>
2025-06-10 23:45:05 +0900

    ナビゲーションバー左端にキャンセルボタン、右端に削除ボタンを配置

--- OkeikoSitter/SettingViewController.swift
+++ OkeikoSitter/SettingViewController.swift
@@ -11,15 +11,34 @@ class SettingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureBarButtonItems()
+        
+        // 左端のキャンセルボタン（アイコン）
+        let cancelImage = UIImage(named: "cancel")
+        let cancelButton = UIBarButtonItem(image: cancelImage, style: .plain, target: self, action: #selector(cancelButtonPressed(_:)))
+        navigationItem.leftBarButtonItem = cancelButton
+        
+        // 右端の削除ボタン（アイコン）
+        let deleteImage = UIImage(named: "delete")
+        let deleteButton = UIBarButtonItem(image: deleteImage, style: .plain, target: self, action: #selector(deleteButtonPressed(_:)))
+        navigationItem.rightBarButtonItem = deleteButton
+    }
+    
+    @objc func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @objc func deleteButtonPressed(_ sender: UIBarButtonItem) {
+        print("ユーザーが削除されます")
     }
 }
-    // MARK: - IBActions
-    
+
+// MARK: - IBActions
 
 
-   // MARK: - Other Methods
+
+// MARK: - Other Methods
 
 private func configureBarButtonItems() {
     
 }
+
