946013ef682744a9627d26f8b146639a35a66feb
Tomoko Tajima <karintococonut777@gmail.com>
2025-06-16 19:39:41 +0900

    ユーザー画像、ユーザー名ラベル、ユーザー選択ボタンの登録、IBOutlet、IBAction接続

--- OkeikoSitter/HomeUserTableViewCell.swift
+++ OkeikoSitter/HomeUserTableViewCell.swift
@@ -7,17 +7,31 @@
 
 import UIKit
 
+///ユーザー登録画面のセル
 class HomeUserTableViewCell: UITableViewCell {
-
+    // MARK: - IBOutlets
+    ///ユーザーイメージビュー
+    @IBOutlet private weak var UserImageView: UIImageView!
+    ///ユーザー名ラベル
+    @IBOutlet private weak var UserNameLabel: UILabel!
+    
+    
+    // MARK: - View Life-Cycle Methods
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    // MARK: - IBActions
+    
+    ///ユーザー選択ボタン
+    @IBAction private func UserSelectionButtonTapped(_ sender: Any) {
     }
     
 }
