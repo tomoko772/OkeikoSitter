461310a6b33dae44e4322ce53f117ced19f7316b
Tomoko Tajima <karintococonut777@gmail.com>
2025-06-16 18:52:02 +0900

    HomeUserTableViewCellクラスの登録

---
+++ OkeikoSitter/HomeUserTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeUserTableViewCell.swift
+//  OkeikoSitter
+//
+//  Created by Tomoko T. Nakao on 2025/06/16.
+//
+
+import UIKit
+
+class HomeUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
