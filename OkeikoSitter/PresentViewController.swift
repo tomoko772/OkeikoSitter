5592442042c2e87b4ace77ce7149b188411385f2
Tomoko Tajima <karintococonut777@gmail.com>
2025-06-25 22:04:47 +0900

    制約の設定変更、IBOutlett接続、IBAction接続

--- OkeikoSitter/PresentViewController.swift
+++ OkeikoSitter/PresentViewController.swift
@@ -11,24 +11,32 @@ import UIKit
 class PresentViewController: UIViewController {
     // MARK: - IBOutlets
     
+    @IBOutlet private weak var hiddenPlaceImageView: UIImageView!
+    @IBOutlet private weak var presentMarkImageView: UIImageView!
+    @IBOutlet private weak var hiddenPlaceLabel: UILabel!
+    @IBOutlet private weak var quesitonMarkImageView: UIImageView!
     
     // MARK: - View Life-Cycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBarButtonItems()
-        
+        configureUI()
     }
     
     // MARK: - IBActions
     
+    @IBAction private func cameraButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction private func registrationButtonTapped(_ sender: Any) {
+    }
     
     // MARK: - Other Methods
     
-    
     private func configureBarButtonItems(){
         // 左端のキャンセルボタン（アイコン）
-        let backImage = UIImage(named: "arrow_back")
+        let backImage = UIImage(named: "cancel")
         let backButton = UIBarButtonItem(image: backImage,
                                          style: .plain,
                                          target: self,
@@ -38,6 +46,10 @@ class PresentViewController: UIViewController {
     
     @objc func backlButtonPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    private func configureUI() { hiddenPlaceImageView.isHidden = true
+        hiddenPlaceLabel.isHidden = true
     }
     
 }
