ef3871baa9ea557eed011e8bc73e9ef36e9958be
Tomoko Tajima <karintococonut777@gmail.com>
2025-07-21 13:53:41 +0900

    ログイン画面の作成

---
+++ OkeikoSitter/View/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  OkeikoSitter
+//
+//  Created by Tomoko T. Nakao on 2025/07/15.
+//
+
+import UIKit
+import SwiftGifOrigin
+import FirebaseAuth
+
+/// 最初の画面
+final class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
+
+    
+    // MARK: - IBOutlets
+    
+    /// プレゼントGIF画像
+    @IBOutlet private weak var presentGIFImageView: UIImageView!
+    ///  Eメール
+    @IBOutlet private weak var emailTextField: UITextField!
+    /// パスワード
+    @IBOutlet private weak var passwordTextField: UITextField!
+    
+    // MARK: - View Life-Cycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presentGIFImageView.loadGif(name: "present")
+        configureBarButtonItems()
+    }
+    
+    // MARK: - IBActions
+    
+    /// 登録ボタンをタップ
+    @IBAction func RegistrationButtonTapped(_ sender: Any) {
+            
+    }
+    
+    // MARK: - Other Methods
+    
+    private func configureBarButtonItems() {
+        // 左端のキャンセルボタン（アイコン）
+        let cancelImage = UIImage(named: "cancel")
+        let cancelButton = UIBarButtonItem(image: cancelImage,
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(cancelButtonPressed(_:)))
+        navigationItem.leftBarButtonItem = cancelButton
+    }
+    
+    @objc func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
