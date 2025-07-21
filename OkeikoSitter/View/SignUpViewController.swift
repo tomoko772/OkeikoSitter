bc99a7106e5fcdc13ede0750240cb15f12921591
Tomoko Tajima <karintococonut777@gmail.com>
2025-07-21 11:11:19 +0900

    キャンセルボタンの追加

--- OkeikoSitter/View/SignUpViewController.swift
+++ OkeikoSitter/View/SignUpViewController.swift
@@ -10,7 +10,7 @@ import SwiftGifOrigin
 
 /// 最初の画面
 final class SignUpViewController: UIViewController {
-
+    
     // MARK: - IBOutlets
     
     /// プレゼントGIF画像
@@ -21,10 +21,11 @@ final class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         presentGIFImageView.loadGif(name: "present")
+        configureBarButtonItems()
     }
-
+    
     // MARK: - IBActions
-   
+    
     /// ログインボタンをタップ
     @IBAction private func loginButtonTapped(_ sender: Any) {
     }
@@ -33,4 +34,19 @@ final class SignUpViewController: UIViewController {
     @IBAction private func signUpButtonTapped(_ sender: Any) {
     }
     
+    // MARK: - Other Methods
+    
+    private func configureBarButtonItems() {
+        // 左端のキャンセルボタン（アイコン）
+        let cancelImage = UIImage(named: "cancel")
+        let cancelButton = UIBarButtonItem(image: cancelImage,
+                                           style: .plain,
+                                           target: self,
+                                           action: #selector(cancelButtonPressed(_:)))
+        navigationItem.leftBarButtonItem = cancelButton
+    }
+    
+    @objc func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
 }
